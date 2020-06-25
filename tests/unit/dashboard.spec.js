ed89bc068aca981ed968fd503b2463c147908339
lewism <lewis@sendy.co.ke>
2020-06-25 21:55:58 +0300

    Fix failing tests

--- tests/unit/dashboard.spec.js
+++ tests/unit/dashboard.spec.js
@@ -8,69 +8,106 @@ describe('Dashboard.vue', () => {
   const wrapper = shallowMount(Dashboard, {
     sync: false,
   });
-  wrapper.vm.dataResponse = {
-    status: true,
-    response: {
-      cash_made_this_month: 'KES -2600.00',
-      cash_made_this_week: 'KES -400',
-      average_rating_this_month: 0,
-      average_rating_this_week: 0,
-      trips_made_this_month: 358,
-      trips_made_this_week: 0,
-      hours_online_this_month: 5.2331,
-      hours_online_this_week: 0,
-      next_transfer: 'KES 3786948.20',
-      monthly_earnings: [
+  wrapper.vm.activeCurrency = 'KES';
+  wrapper.vm.currencyStats = {
+    KES: {
+      cashMadeThisMonth: 100,
+      cashMadeThisWeek: 200,
+      averageTripsThisMonth: 30,
+      averageTripsThisWeek: 10,
+      nextTransfer: '3779948.20',
+      monthlyEarnings: [
         {
-          amount: 98209,
+          amount: 96978,
           month: 'February',
-          default_currency: 'KES',
+          currency: 'KES',
         },
         {
           amount: 11988,
           month: 'March',
-          default_currency: 'KES',
+          currency: 'KES',
         },
         {
           amount: 1500,
           month: 'April',
-          default_currency: 'KES',
+          currency: 'KES',
         },
       ],
-      total_riders: 9,
+      totalOrders: [
+        {
+          totalOrders: 6,
+          currency: 'KES',
+          month: 'April',
+        },
+        {
+          totalOrders: 15,
+          currency: 'KES',
+          month: 'February',
+        },
+        {
+          totalOrders: 79,
+          currency: 'KES',
+          month: 'January',
+        },
+        {
+          totalOrders: 3,
+          currency: 'KES',
+          month: 'March',
+        },
+        {
+          totalOrders: 222,
+          currency: 'KES',
+          month: 'May',
+        },
+      ],
     },
   };
+  wrapper.vm.ownerStats = {
+    averageRatingThisWeek: 3,
+    averageRatingThisMonth: 4,
+    hoursOnlineThisMonth: 0.0081,
+    hoursOnlineThisWeek: 0.0081,
+  };
   it('Check whether cash made this month function always returns a positive', () => {
-    expect(wrapper.vm.cashMadeThisMonth()).equal('KES 2,600');
+    expect(wrapper.vm.cashMadeThisMonth).equal(100);
   });
   it('Check whether cash made this week function always returns a positive', () => {
-    expect(wrapper.vm.cashMadeThisWeek()).equal('KES 400');
+    expect(wrapper.vm.cashMadeThisWeek).equal(200);
   });
   it('Check whether next transfer function always returns a truncated positive', () => {
-    expect(wrapper.vm.nextTransfer()).equal('KES 3,786,948');
+    expect(wrapper.vm.nextTransfer).equal(3779948.2);
   });
-  it('Check whether weekly rating function always truncates the rating value', () => {
-    expect(wrapper.vm.ratingThisWeek()).equal(0);
+  it('Check whether weekly rating function always returns the correct rating array', () => {
+    expect(wrapper.vm.ratingThisWeek()[0]).equal(1);
+    expect(wrapper.vm.ratingThisWeek()[1]).equal(1);
+    expect(wrapper.vm.ratingThisWeek()[2]).equal(1);
+    expect(wrapper.vm.ratingThisWeek()[3]).equal(0);
+    expect(wrapper.vm.ratingThisWeek()[4]).equal(0);
   });
-  it('Check whether monthly rating function always truncates the rating value', () => {
-    expect(wrapper.vm.ratingThisMonth()).equal(0);
+  it('Check whether monthly rating function always returns the correct rating array', () => {
+    expect(wrapper.vm.ratingThisMonth()[0]).equal(1);
+    expect(wrapper.vm.ratingThisMonth()[1]).equal(1);
+    expect(wrapper.vm.ratingThisMonth()[2]).equal(1);
+    expect(wrapper.vm.ratingThisMonth()[3]).equal(1);
+    expect(wrapper.vm.ratingThisMonth()[4]).equal(0);
   });
-  it('Check whether weekly online hours function always truncates the hours', () => {
-    expect(wrapper.vm.hoursOnlineThisWeek()).equal(0);
+  it('Check whether weekly online hours function always returns the correct the hours', () => {
+    expect(wrapper.vm.hoursOnlineThisWeek()).equal(0.0081);
   });
-  it('Check whether monthly online hours function always truncates the hours', () => {
-    expect(wrapper.vm.hoursOnlineThisMonth()).equal(5);
+  it('Check whether monthly online hours function always returns the correct the hours', () => {
+    expect(wrapper.vm.hoursOnlineThisMonth()).equal(0.0081);
   });
-  it('Check whether weekly rating percentage function always calculates the correct percentage', () => {
-    expect(wrapper.vm.ratingPercentage()).equal(0);
+  it('Check whether currencies function always returns the correct currencies array', () => {
+    expect(wrapper.vm.currencies[0]).equal('KES');
   });
-  it('Check whether monthly rating percentage function always calculates the correct percentage', () => {
-    expect(wrapper.vm.ratingPercentageMonth()).equal(0);
+  it('Check whether ordersDataPoints function always calculates the correct data for the chart', () => {
+    expect(wrapper.vm.ordersDataPoints.datasets[0].label).equal('Total Orders');
+    expect(wrapper.vm.ordersDataPoints.labels[0]).equal('April');
+    expect(wrapper.vm.ordersDataPoints.datasets[0].data[0]).equal(6);
   });
-  it('Check whether weekly hours online percentage function always calculates the correct percentage', () => {
-    expect(wrapper.vm.hoursPercentageWeek()).equal(0);
-  });
-  it('Check whether monthly hours online percentage function always calculates the correct percentage', () => {
-    expect(wrapper.vm.hoursPercentageMonth()).equal(Math.floor((wrapper.vm.dataResponse.response.hours_online_this_month / (moment().daysInMonth() * 24)) * 100));
+  it('Check whether revenueDataPoints function always calculates the correct data for the chart', () => {
+    expect(wrapper.vm.revenueDataPoints.datasets[0].label).equal('Amount earned (KES)');
+    expect(wrapper.vm.revenueDataPoints.labels[0]).equal('February');
+    expect(wrapper.vm.revenueDataPoints.datasets[0].data[0]).equal(96978);
   });
 });
