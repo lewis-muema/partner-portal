c0fe4dfb8d040cd182ec79e63aeb1d5a08842da2
victor misiko <victormisiko.vm@gmail.com>
2021-03-24 17:18:44 +0300

    Added strings for truckvalidationMixin translations

--- src/locales/locales/en.js
+++ src/locales/locales/en.js
@@ -1,9 +1,11 @@
 import appHeader from '../headers/en/appHeader';
 import footer from '../headers/en/footer';
+import truckValidationMxn from '../truckValidationMixin/en';
 
 const translation = {
     appHeader,
     footer,
+    truckValidationMxn,
 };
 
 export default translation;
--- src/locales/locales/fr.js
+++ src/locales/locales/fr.js
@@ -1,9 +1,11 @@
 import appHeader from '../headers/fr/appHeader';
 import footer from '../headers/fr/footer';
+import truckValidationMxn from '../truckValidationMixin/en';
 
 const translation = {
     appHeader,
     footer,
+    truckValidationMxn,
 };
 
 export default translation;
