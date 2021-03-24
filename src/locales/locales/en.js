0783d8f8c4cc48c2c26a6064d6c816c7b6b5c1e0
victor misiko <victormisiko.vm@gmail.com>
2021-03-24 19:20:01 +0300

    importe resourcefiles for authlogin and available orders component

--- src/locales/locales/en.js
+++ src/locales/locales/en.js
@@ -1,11 +1,15 @@
 import appHeader from '../headers/en/appHeader';
 import footer from '../headers/en/footer';
 import truckValidationMxn from '../truckValidationMixin/en';
+import authLogin from '../authLogin/en';
+import availableOrders from '../availableOrders/en';
 
 const translation = {
     appHeader,
     footer,
     truckValidationMxn,
+    authLogin,
+    availableOrders,
 };
 
 export default translation;
