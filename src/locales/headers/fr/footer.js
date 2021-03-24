520d9f4e21c1ec438f20ae9f127daa5089144874
victor misiko <victormisiko.vm@gmail.com>
2021-03-24 16:30:54 +0300

    Added header and footer resource files

---
+++ src/locales/headers/fr/footer.js
@@ -0,0 +1,12 @@
+const fr = {
+    how_likely: 'FR_How likely are you to invite a friend or collegue to join Sendy?',
+    not_likely: 'FR_0 Not Likely',
+    very_likely: 'FR_10 Very Likely',
+    what_like_most: 'FR_What do you like most about Sendy?(Optional)',
+    submit: 'FR_Submit',
+    thank_you: 'FR_Thank you. We will use your feeedback to improve our service.',
+    something_went_wrong: 'FR_Sorry Something went wrong. Try Again',
+    user_already_surveyed: 'FR_User already surveyed within grace period.',
+};
+
+export default fr;
