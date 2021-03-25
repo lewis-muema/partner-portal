f57ade3e681ec3c073a73a957e23120cfd006426
victor misiko <victormisiko.vm@gmail.com>
2021-03-25 23:15:29 +0300

    added exteranlTracking resource Files

---
+++ src/locales/externalTrackin/en.js
@@ -0,0 +1,15 @@
+const translation = {
+    decrease_speed: 'Decrease speed',
+    rewind: 'Rewind',
+    play: 'Play',
+    pause: 'Pause',
+    fast_forward: 'Fast forward',
+    increase_speed: 'Increase speed',
+    no_data_returned: 'No data has been returned',
+    provide_all: 'Please provide all the parameters',
+    decrease: 'Decrease',
+    fast: 'Fast',
+    increase: 'Increase',
+};
+
+export default translation;
