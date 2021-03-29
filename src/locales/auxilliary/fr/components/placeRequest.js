5a56cc5ab62ec9ab2dcd16d6b06ae409a03e8862
victor misiko <victormisiko.vm@gmail.com>
2021-03-29 16:49:32 +0300

    Added auxillary resource files

---
+++ src/locales/auxilliary/fr/components/placeRequest.js
@@ -0,0 +1,25 @@
+const translation = {
+    pickup_location: 'FR_pickup location',
+    destination: 'FR_destination',
+    distance: 'FR_distance',
+    pickup_date: 'FR_pickup date',
+    order_number: 'FR_order number',
+    truck: 'FR_truck',
+    price: 'FR_price',
+    vat: 'FR_VAT',
+    action: 'FR_action',
+    pickup: 'FR_Pickup',
+    destiantion_capital: 'FR_Destination',
+    pick_up_date: 'FR_Pick-up date',
+    amount: 'FR_Amount',
+    in_transit: 'FR_in transit',
+    maximum_of: 'FR_You can request a maximum of',
+    total_amount: 'FR_Total amount',
+    choose_fueling_station: 'FR_Choose fueling Station',
+    station_address: 'FR_Station address',
+    fuel_type: 'FR_Fuel type',
+    confirm_request: 'FR_Confirm request',
+    no_requests: 'FR_There are no orders',
+};
+
+export default translation;
