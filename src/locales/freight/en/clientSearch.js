abcf0459251a091fb4b3fd48ce4817b9c46bb19e
victor misiko <victormisiko.vm@gmail.com>
2021-03-30 16:34:39 +0300

    Added Frieght strings in the freight resource Files

---
+++ src/locales/freight/en/clientSearch.js
@@ -0,0 +1,8 @@
+const translation = {
+    enter_name: 'Enter name or phone number',
+    client_name: 'Client Name',
+    client_email: 'Client Email',
+    phone_number: 'Phone Number',
+};
+
+export default translation;
