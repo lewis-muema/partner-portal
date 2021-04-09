fc28b01491e8481f2c96f5579059898b682dd9d8
Jeremy Wambugu <jeremy@Jeremys-MacBook-Pro.local>
2021-04-09 16:11:20 +0300

    updated verification response message

--- src/views/BidRecipient/verification.vue
+++ src/views/BidRecipient/verification.vue
@@ -54,7 +54,7 @@ export default {
               this.notify(3, 1, 'Verified successfully');
               this.$router.push('/login');
             } else {
-              this.notify(3, 1, 'Verification code expired');
+              this.notify(3, 1, response.data.message);
             }
           })
           .catch(error => {
