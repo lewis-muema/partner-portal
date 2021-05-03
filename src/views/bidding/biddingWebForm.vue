f50ce01d82e7ada23635a729abe6e7a2e4a17066
Jeremy <jeremywambugu@sendyit.com>
2021-05-03 17:51:22 +0300

    changing env to prod

--- src/views/bidding/biddingWebForm.vue
+++ src/views/bidding/biddingWebForm.vue
@@ -469,9 +469,10 @@ export default {
           } else {
             this.submitted = true;
           }
-
           if (res.status === 200) {
-            if (process.env.DOCKER_ENV === 'development') {
+            console.log('inside');
+            console.log(process.env.DOCKER_ENV);
+            if (process.env.DOCKER_ENV === 'production') {
               mixpanel.track('Shipment Request Viewed', {
                 transporterId: res.data.data.quotation.transporter_id,
                 phone: res.data.data.transporter_user_phone,
