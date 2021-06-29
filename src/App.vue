51ba2965c95fe1940a0fdef61ebe6b9983af8308
lewism <lewis@sendy.co.ke>
2021-06-29 13:24:06 +0300

    Add freshchat widget for owners

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,98 @@ export default {
     appHeader,
     loginHeader,
   },
+  watch: {
+    $route(to, from) {
+      if ((from.path === '/login' && to.path === '/') || to.path === '/login') {
+        this.initializeFreshChat();
+      }
+    },
+  },
+  mounted() {
+    this.initializeFreshChat();
+  },
+  methods: {
+    initializeFreshChat() {
+      let session = null;
+      if (localStorage.sessionData) {
+        session = JSON.parse(localStorage.sessionData).payload;
+      }
+      const restoreIds = localStorage.userRestoreIds ? JSON.parse(localStorage.userRestoreIds) : [];
+      if (document.getElementById('Freshchat-js-sdk')) {
+        window.fcWidget.user.clear();
+        window.fcWidget.destroy();
+        document.getElementById('Freshchat-js-sdk').remove();
+      }
+      setTimeout(() => {
+        if (session && this.$route.path !== '/login') {
+          const idArray = restoreIds.filter(data => data.email === session.email);
+          const id = idArray.length > 0 ? idArray[0].id : null;
+          this.createFreshChatScript(session.email, id, session);
+        } else {
+          this.createFreshChatScript();
+        }
+      }, 1000);
+    },
+    createFreshChatScript(userEmail, restoreID, session) {
+      const script = document.createElement('script');
+        script.id = 'Freshchat-js-sdk';
+        script.onload = () => {
+          const payload = {
+            token: '88605441-3539-4e90-9e64-0fb1e4b1736f',
+            host: 'https://wchat.freshchat.com',
+            ...(userEmail && { externalId: userEmail }),
+            ...(restoreID && { restoreId: restoreID }),
+            config: {
+              headerProperty: {
+                backgroundColor: '#EE7D00',
+                foregroundColor: '#FFFFFF',
+              },
+            },
+          };
+          if (this.$route.path === '/login') {
+            window.fcWidget.init(payload);
+          } else {
+            window.fcWidget.init(payload);
+            this.setFreshChatRestoreIds(session, restoreID);
+          }
+        };
+        script.src = 'https://wchat.freshchat.com/js/widget.js';
+        document.head.appendChild(script);
+    },
+    setFreshChatRestoreIds(session, restoreID) {
+      if (!restoreID && session) {
+        window.fcWidget.user.setProperties({
+          firstName: session.name,
+          email: session.email,
+          phone: session.phone,
+        });
+      }
+      window.fcWidget.on('user:created', (resp) => {
+        const status = resp && resp.status;
+            const data = resp && resp.data;
+        if (status === 200) {
+          if (data.restoreId) {
+            if (localStorage.userRestoreIds) {
+              const ids = JSON.parse(localStorage.userRestoreIds);
+              const states = ids.filter(info => info.id === data.restoreId);
+              if (states.length === 0) {
+                ids.push({
+                  email: session.email,
+                  id: data.restoreId,
+                });
+              }
+              localStorage.userRestoreIds = JSON.stringify(ids);
+            } else {
+              localStorage.userRestoreIds = JSON.stringify([{
+                email: session.email,
+                id: data.restoreId,
+              }]);
+            }
+          }
+        }
+      });
+    },
+  },
 };
 </script>
 
